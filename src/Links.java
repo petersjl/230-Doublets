72c744f9261ed4fa5cc237941cb5c6d3250f06df
hyunj <hyunj@rose-hulman.edu>
2018-12-15 21:40:39 -0500

    DONE

--- src/Links.java
+++ src/Links.java
@@ -1,33 +1,66 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Links implements LinksInterface {
 
-	public Links(String filename){
-		Scanner sc = null;
-		File f= new File(filename);
-		try{
-			sc = new Scanner(f);
-		} catch (FileNotFoundException e){
+public class Links {
+	
+	private HashMap<String, HashSet<String>> map;
+	
+	public Links(String filename) {
+		this.map = new HashMap<String, HashSet<String>>();
+		fileInput(filename);
+	}
+	
+	private void fileInput(String filename) {
+		ArrayList<String> tempList = new ArrayList<String>();
+		File file  = new File(filename);
+		try {
+			Scanner s = new Scanner(file);
+			while (s.hasNext()) {
+				tempList.add(s.next());
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		while(sc.hasNextLine()){
-			String word = sc.next();
-			System.out.println(word);
+		Iterator<String> it = tempList.iterator();
+		while (it.hasNext()) {
+			String temp = it.next();
+			HashSet<String> builder = new HashSet<String>();
+			for (String t : tempList) {
+				if (t != temp && t.length() == temp.length()) {
+					char[] ch1 = t.toCharArray();
+					char[] ch2 = temp.toCharArray();
+					int count = 0;
+					for (int i = 0; i < t.length(); i++) {
+						if (ch1[i] != ch2[i]) {
+							count++;
+						}
+						if (count >= 2) {
+							break;
+						}
+					}
+					if (count < 2) {
+						builder.add(t);
+					}
+				}
+			}
+			this.map.put(temp, builder);
 		}
 	}
-	@Override
-	public Set<String> getCandidates(String word) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public Set<String> getCandidates(String s) {
+		if (!this.map.containsKey(s) || this.map.get(s).isEmpty()) {
+			return null;
+		}
+		return this.map.get(s);
 	}
-
-	@Override
-	public boolean exists(String word) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	
 }
+
